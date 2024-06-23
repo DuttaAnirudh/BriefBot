e85202d5dde0dd5ecea83323c7f2187ee5ebb592
DuttaAnirudh <worktodutta@gmail.com>
2024-06-23 21:36:09 +0530

    build hero section and input section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,33 @@
+import { logo } from "../index";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="BriefBot Logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() =>
+            window.open("https://github.com/DuttaAnirudh/BriefBot")
+          }
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className="desc">
+        Simplify your reading with BriefBot, an article summarizer that
+        transforms lengthy articles into clear and concise summaries.
+      </h2>
+    </header>
+  );
 };
 
 export default Hero;
